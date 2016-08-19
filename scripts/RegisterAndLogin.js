337efc8d0c48d4654d33985b0a27f5ce6c85818a
Nedyalko Kikov <inktech.n.kikov@gmail.com>
2016-08-19 18:35:29 +0300

    Update register plus login
    409 (conflict)

--- scripts/RegisterAndLogin.js
+++ scripts/RegisterAndLogin.js
@@ -3,37 +3,34 @@ const kinveyAppID = 'kid_By6NFYOt';
 const appSecrets = '9887cd73e9a84e26875688775c120525';
 
 function login() {
-    let authBase64 = btoa(kinveyAppID + ":" + appSecrets);
-    let loginUrl = kinveyBaseUrl + "user/" + kinveyAppID + "/login";
     let loginData = {
-        username: $("#loginUser").val(),
-        password: $("#loginPass").val()
+        username: $('#loginUser').val(),
+        password: $('#loginPass').val()
     };
     $.ajax({
         method: "POST",
-        url: loginUrl,
+        url: kinveyBaseUrl + 'user/' + kinveyAppID + '/login',
         data: loginData,
-        headers: {"Authorization": "Basic" + " " + authBase64},
-    })
-
-}
-function register() {
-    let authBase64 = btoa(kinveyAppID + ":" + appSecrets);
-    let registerUrl = kinveyBaseUrl + "user/" + kinveyAppID + "/";
-    let registerData = {
-        fullname: $("#fullName").val(),
-        username: $("#userName").val(),
-        password: $("#passInput").val(),
-        passConfirm: $("#passwordConfirm").val()
-    };
-    $.ajax({
-        method: "POST",
-        url: registerUrl,
-        data: registerData,
-        headers: {"Authorization": "Basic" + " " + authBase64},
+        headers: {"Authorization": "Basic " + btoa(kinveyAppID + ":" + appSecrets)}
     });
 }
-$(function () {
+function register() {
+    let registerData = {
+        name: $('#fullName').val(),
+        username: $('#userName').val(),
+        password: $('#passInput').val(),
+        passwordConf: $('#passwordConfirm').val()
+    };
+    $.ajax({
+        method: "POST",
+        url: kinveyBaseUrl + 'user/' + kinveyAppID + '/',
+        data: registerData,
+        headers:{
+            "Authorization": "Basic " + btoa(kinveyAppID + ":" + appSecrets)
+        }
+    });
+}
+$(function() {
     $('#formLogin').submit(function(e){e.preventDefault(); login()});
     $('#formRegister').submit(function(e){e.preventDefault(); register()});
 });

a6e05f231431c4d6195aa2829b785831aff631c0
Nedyalko Kikov <inktech.n.kikov@gmail.com>
2016-08-25 19:22:42 +0300

    Set homework in teachers page -- done!

--- scripts/teachersSetHomeWork.js
+++ scripts/teachersSetHomeWork.js
@@ -1,3 +1,21 @@
 const kinveyBaseUrl = 'https://baas.kinvey.com/';
 const kinveyAppID = 'kid_By6NFYOt';
 const appSecrets = '9887cd73e9a84e26875688775c120525';
+
+function setHomerowk() {
+    let homeWorkUrl =  kinveyBaseUrl + "appdata/" + kinveyAppID + "/Teachers";
+    let authHeaders = {
+        "Authorization": "Kinvey " + sessionStorage.getItem('authToken'),
+    };
+    let homeWorkData = {
+        name: $("#simpleName").val(),
+        subject: $("#subject").val(),
+        description: $("#description").val()
+    };
+    $.ajax({
+        method: "POST",
+        url:homeWorkUrl,
+        data:homeWorkData,
+        headers:authHeaders,
+    })
+}

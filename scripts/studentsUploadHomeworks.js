82acd18b9ea2b29667c6ccc6712b64111952ebb4
Nedyalko Kikov <inktech.n.kikov@gmail.com>
2016-08-26 15:19:41 +0300

    Fixed!

--- scripts/studentsUploadHomeworks.js
+++ scripts/studentsUploadHomeworks.js
@@ -1,14 +1,14 @@
 const kinveyBaseUrl = 'https://baas.kinvey.com/';
 const kinveyAppID = 'kid_By6NFYOt';
 function uploadHomeworks() {
-    let setHomeWorkUrl =  kinveyBaseUrl + "appdata/" + kinveyAppID + "/Students";
+    let setHomeWorkUrl =  kinveyBaseUrl + "appdata/" + kinveyAppID + "/Homeworks";
     let authHeaders = {
         "Authorization": "Kinvey " + sessionStorage.getItem('authToken'),
     };
     let upload = {
         name:$('#studentName').val(),
-        theme:$('#theme').val(),
-        description:$('#homeWorkDescription').val(),
+        theme:$('#themeOfHomework').val(),
+        description:$('#descriptionOfHomeWork').val(),
     };
     $.ajax({
         method: "POST",

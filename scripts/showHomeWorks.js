c7c6d1147c34e14114b8bf811d9a7e48aa5bd810
Nedyalko Kikov <inktech.n.kikov@gmail.com>
2016-08-27 16:34:25 +0300

    change functions

--- scripts/showHomeWorks.js
+++ scripts/showHomeWorks.js
@@ -1,12 +1,10 @@
-const kinveyBaseUrl = 'https://baas.kinvey.com/';
-const kinveyAppID = 'kid_By6NFYOt';
- //const appSecrets = '9887cd73e9a84e26875688775c120525';
+
 function showHomeW() {
     let method = "GET";
     let userAtuh = "Kinvey " + sessionStorage.getItem('authToken');
     let headers = {};
     headers['Authorization'] = userAtuh;
-    let requestUrl = kinveyBaseUrl + "appdata/" + kinveyAppID + "/Homeworks";
+    let requestUrl = kinveyBaseUrl + "appdata/" + kinveyAppID + "/Teachers";
     let request = {
         method:method,
         headers:headers,
@@ -15,7 +13,4 @@ function showHomeW() {
     $.ajax(request).then(function (response) {
        alert(JSON.stringify(response));
     });
-}
-function showFinished() {
-
 }

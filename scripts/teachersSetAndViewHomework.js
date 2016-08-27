8bfe922a5b941c9fd53e566a5d8ab4bf23bb1dc6
Nedyalko Kikov <inktech.n.kikov@gmail.com>
2016-08-27 21:33:04 +0300

    Validations available - check fields!

--- scripts/teachersSetAndViewHomework.js
+++ scripts/teachersSetAndViewHomework.js
@@ -2,22 +2,41 @@ const kinveyBaseUrl = 'https://baas.kinvey.com/';
 const kinveyAppID = 'kid_By6NFYOt';
 const appSecrets = '9887cd73e9a84e26875688775c120525';
 
-function setHomerowk() {
-    let homeWorkUrl =  kinveyBaseUrl + "appdata/" + kinveyAppID + "/Teachers";
-    let authHeaders = {
-        "Authorization": "Kinvey " + sessionStorage.getItem('authToken'),
-    };
-    let homeWorkData = {
-        name: $("#simpleName").val(),
-        subject: $("#subject").val(),
-        description: $("#description").val()
-    };
-    $.ajax({
-        method: "POST",
-        url:homeWorkUrl,
-        data:homeWorkData,
-        headers:authHeaders,
-    })
+function checkFields() {
+    let nameOfTeach = $('#simpleName').val();
+    let subj = $('#subject').val();
+    let descrHomeWork = $('#descrHomeWork').val();
+    if (nameOfTeach === "") {
+        alert("Please insert name of teacher.Try again");
+    }
+    else if (subj === "") {
+        alert("Please input subject field.Try again");
+    }
+    else if(descrHomeWork.length <= 2){
+        alert("The description is too short!");
+    }
+    else {
+        setHomerowk();
+    }
+
+    function setHomerowk() {
+        let homeWorkUrl = kinveyBaseUrl + "appdata/" + kinveyAppID + "/Teachers";
+        let authHeaders = {
+            "Authorization": "Kinvey " + sessionStorage.getItem('authToken'),
+        };
+        let homeWorkData = {
+            name: $("#simpleName").val(),
+            subject: $("#subject").val(),
+            description: $("#descrHomeWork").val()
+        };
+
+        $.ajax({
+            method: "POST",
+            url: homeWorkUrl,
+            data: homeWorkData,
+            headers: authHeaders,
+        });
+    }
 }
 function showHomeW() {
     let method = "GET";

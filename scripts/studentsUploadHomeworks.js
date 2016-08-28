accd6a91ed45143bed67dc1ea8243187bc1a202b
Nedyalko Kikov <inktech.n.kikov@gmail.com>
2016-08-29 00:35:26 +0300

    update - teachers ans students

--- scripts/studentsUploadHomeworks.js
+++ scripts/studentsUploadHomeworks.js
@@ -15,75 +15,73 @@ function studentsValidation() {
     let nameOfStudent = $('#studentName').val();
     let nameOfTheme = $('#themeOfHomework').val();
     let descrOfHmrkSt = $('#descriptionOfHomeWork').val();
-    if(nameOfStudent === ""){
+    if (nameOfStudent === "") {
         showInfoStuden("Name field cant be empty");
     }
-    else if(nameOfTheme === ""){
+    else if (nameOfTheme === "") {
         showInfoStuden("Theme field cant be empty");
     }
-    else if(descrOfHmrkSt.length <= 4){
+    else if (descrOfHmrkSt.length <= 4) {
         showInfoStuden("Homework is too short!");
     }
     else {
         uploadHomeworks();
     }
 }
+    function uploadHomeworks() {
+        let setHomeWorkUrl = kinveyBaseUrl + "appdata/" + kinveyAppID + "/Homeworks";
+        let authHeaders = {
+            "Authorization": "Kinvey " + sessionStorage.getItem('authToken'),
+        };
 
-function uploadHomeworks() {
-    let setHomeWorkUrl =  kinveyBaseUrl + "appdata/" + kinveyAppID + "/Homeworks";
-    let authHeaders = {
-        "Authorization": "Kinvey " + sessionStorage.getItem('authToken'),
-    };
+        let upload = {
+            name: $('#studentName').val(),
+            theme: $('#themeOfHomework').val(),
+            description: $('#descriptionOfHomeWork').val(),
+        };
+        $.ajax({
+            method: "POST",
+            url: setHomeWorkUrl,
+            data: upload,
+            headers: authHeaders,
+            success: showInfoStuden,
 
-    let upload = {
-        name:$('#studentName').val(),
-        theme:$('#themeOfHomework').val(),
-        description:$('#descriptionOfHomeWork').val(),
-    };
-    $.ajax({
-        method: "POST",
-        url:setHomeWorkUrl,
-        data:upload,
-        headers:authHeaders,
-        success:showInfoStuden,
-
-    });
-
-}
-$(document).ready(function () {
-    $('#showHomeWorkStudents').click(function () {
-        $('#fieldInStudents').show();
-    });
-    let method = "GET";
-    let userAtuh = "Kinvey " + sessionStorage.getItem('authToken');
-    let headers = {};
-    headers['Authorization'] = userAtuh;
-    let requestUrl = kinveyBaseUrl + "appdata/" + kinveyAppID + "/Teachers";
-    let request = {
-        method:method,
-        headers:headers,
-        url:requestUrl,
-    };
-    $.ajax(request).then(function (response) {
-        for(let obj of response){
-            let list = $('#fieldInStudents');
-            let inner = document.createElement('ul');
-            let nameOfTeacher = document.createElement('li');
-            let subj = document.createElement('li');
-            let descr = document.createElement('ul');
-
-            nameOfTeacher.appendChild(document.createTextNode(obj.name));
-            subj.appendChild(document.createTextNode(obj.subject));
-            descr.appendChild(document.createTextNode(obj.description));
-            inner.appendChild(nameOfTeacher);
-            inner.appendChild(subj);
-            inner.appendChild(descr);
-            list.append(inner);
-        }
-    });
-    $(document).ready(function () {
-        $('#hideHomeworksStudents').click(function () {
-            $('#fieldInStudents').hide();
         });
-    });
-});
+    }
+        $(document).ready(function () {
+            $('#showHomeWorkStudents').click(function () {
+                $('#fieldInStudents').show();
+            });
+            let method = "GET";
+            let userAtuh = "Kinvey " + sessionStorage.getItem('authToken');
+            let headers = {};
+            headers['Authorization'] = userAtuh;
+            let requestUrl = kinveyBaseUrl + "appdata/" + kinveyAppID + "/Teachers";
+            let request = {
+                method: method,
+                headers: headers,
+                url: requestUrl,
+            };
+            $.ajax(request).then(function (response) {
+                for (let obj of response) {
+                    let list = $('#fieldInStudents');
+                    let inner = document.createElement('ul');
+                    let nameOfTeacher = document.createElement('li');
+                    let subj = document.createElement('li');
+                    let descr = document.createElement('ul');
+
+                    nameOfTeacher.appendChild(document.createTextNode(obj.name));
+                    subj.appendChild(document.createTextNode(obj.subject));
+                    descr.appendChild(document.createTextNode(obj.description));
+                    inner.appendChild(nameOfTeacher);
+                    inner.appendChild(subj);
+                    inner.appendChild(descr);
+                    list.append(inner);
+                }
+            });
+            $(document).ready(function () {
+                $('#hideHomeworksStudents').click(function () {
+                    $('#fieldInStudents').hide();
+                });
+            });
+        });

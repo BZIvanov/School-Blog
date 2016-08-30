fe4aa1f7f1823a4654e901e419cda36149c61de4
Nedyalko Kikov <inktech.n.kikov@gmail.com>
2016-08-30 22:19:11 +0300

    Change in finished-homework

--- scripts/finishedHomeWorks.js
+++ scripts/finishedHomeWorks.js
@@ -3,16 +3,6 @@ $(document).ready(function () {
         $('#showTeachHome').show();
     });
 });
-let method = "GET";
-let userAtuh = "Kinvey " + sessionStorage.getItem('authToken');
-let headers = {};
-headers['Authorization'] = userAtuh;
-let requestUrl = kinveyBaseUrl + "appdata/" + kinveyAppID + "/Teachers";
-let request = {
-    method: method,
-    headers: headers,
-    url: requestUrl,
-};
 let methodShow = "GET";
 let userAtuhStudent = "Kinvey " + sessionStorage.getItem('authToken');
 let headersStudent = {};
@@ -23,23 +13,7 @@ let requestStudent = {
     headers: headersStudent,
     url: requestUrlStudent,
 };
-$.ajax(request).then(function (response) {
-    for (let obj of response) {
-        let list = $('#showTeachHome');
-        let inner = document.createElement('li');
-        let nameOfTeacher = document.createElement('li');
-        let subj = document.createElement('li');
-        let descr = document.createElement('ul');
 
-        nameOfTeacher.appendChild(document.createTextNode(obj.name));
-        subj.appendChild(document.createTextNode(obj.subject));
-        descr.appendChild(document.createTextNode(obj.description));
-        inner.appendChild(nameOfTeacher);
-        inner.appendChild(subj);
-        inner.appendChild(descr);
-        list.append(inner);
-    }
-});
 $.ajax(requestStudent).then(function (responseSt) {
     for (let objSt of responseSt) {
         let listSt = $('#showStHome');

b0770d30c309083aead6a44d72c735d9b043829c
BZIvanov <ivanov.biser@gmx.com>
2021-06-13 13:35:02 +0300

    moved register and login script

---
+++ common/scripts/translations.js
@@ -0,0 +1,30 @@
+const translations = {
+  errorMsg: {
+    en: 'Try again!',
+    bg: 'Опитай пак!',
+  },
+  loginSuccess: {
+    en: 'Login successful',
+    bg: 'Логнахте се успешно',
+  },
+  emptyName: {
+    en: 'Try again.Full name cant be empty',
+    bg: 'Опитай пак! -Име- Не трябва да бъде празно!',
+  },
+  emptyUser: {
+    en: 'Try again.User name cant be empty',
+    bg: 'Опитай пак! -Потребител- Не трябва да бъде празно!',
+  },
+  emptyPassword: {
+    en: 'The password must be minimum 4 symbols',
+    bg: 'Паролата трябва да бъде по-дълга от 4 символа!',
+  },
+  passwordsMismatch: {
+    en: 'The two fields with passwords must be the same!',
+    bg: 'Паролите не съвпадат!',
+  },
+  registerSuccess: {
+    en: 'User registered successfully',
+    bg: 'Регистраран потребител',
+  },
+};
